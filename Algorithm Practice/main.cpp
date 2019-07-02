3fa9f218e25957e15f69549f4c9490c5c24c338d
Donghoon Han <dhk1349@naver.com>
2019-07-02 13:03:31 +0900

    Initial Commit

---
+++ Algorithm Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm Practice
+//
+//  Created by 한동훈 on 02/07/2019.
+//  Copyright © 2019 한동훈. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
