7f6c85e285a602b01b050f39d846aca62d9066b4
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-03-19 19:22:03 +0900

    Update tmax_tmin.py

--- Algorithm Assignment/tmax_tmin.py
+++ Algorithm Assignment/tmax_tmin.py
@@ -8,7 +8,8 @@ Possible errors:
     string as input (o)
     negative number as input (o)
     Non-integer as input  (o)
-    when m is larger than n
+    when m is larger than n (o)
+    n must be 3 or bigger than 3 (o)
     
 """
 import random
@@ -17,22 +18,27 @@ import sys
 print("==Type Two Integers!==",end="")
 error=""
 try:
-    n=input("Type n:")
-    m=input("Type m:")
-    if (n<m):
-        error+="N must be same or bigger than M.\n"
-    if (n<0 or m<0):
-        error+="Both M and N should not be negative number."
+    n=int(input("Type n:"))
+    m=int(input("Type m:"))
+    """
     if (type(n)!=int or type(m)!=int):
-        error+="M or N should be interger format"
+        error+="M or N should be integer format"
+        raise Exception(error)
+    """
+    if (n<m):
+        error+="Error: N must be same or bigger than M.\n"
+    if (n<0 or m<0):
+        error+="Error: Both M and N should not be negative number.\n"
+    if(n<3):
+        error+="Error: N must be 3 or bigger than 3.\n"
     if(error!=""):
-        raise usererror
+        raise Exception(error)
             
 except ValueError:
-    print("You must type numbers.")
+    print("Error: You must type numbers.")
     sys.exit()
-except usererror:
-    print(error)
+except Exception as e:
+    print(e)
     sys.exit()
     
 container=[None]*n
