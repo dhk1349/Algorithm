31ae6e52c1ed9f725f489bdde1ec54c8eb1a7f66
dhk1349 <dhk1349@naver.com>
2020-04-20 00:28:36 +0900

    Update min_matmul.py

--- Algorithm Assignment/min_matmul.py
+++ Algorithm Assignment/min_matmul.py
@@ -5,3 +5,20 @@ Created on Sun Apr 19 23:45:23 2020
 @author: dhk13
 """
 
+def minmul(d, ):
+    M=[[None]*(len(d)-1) for i in range(len(d)-1)]
+    #d-1 is number of matrix
+    #iterates through matrix M
+    for i in range(len(d)-1):
+        M[i][i]=0
+    # i means diff between row and column
+    for i in range(1,len(d)-1):
+        # (j,j+i) is starting matrix
+        for j in range():
+            for k in range(i, i+j+1):
+                M[i][i+j]=
+    
+    return path, M
+
+def order(p, i, j):
+    
